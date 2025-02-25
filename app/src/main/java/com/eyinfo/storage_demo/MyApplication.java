c49f3bd04f2478fe8e6abb6a9e71300ad2ceeee6
ethan0866 <ethan0866@163.com>
2025-02-25 16:16:37 +0800

    fix

--- app/src/main/java/com/eyinfo/storage_demo/MyApplication.java
+++ app/src/main/java/com/eyinfo/storage_demo/MyApplication.java
@@ -2,15 +2,17 @@ package com.eyinfo.storage_demo;
 
 import android.app.Application;
 
-import com.eyinfo.storage_demo.modules.UserModules;
 import com.eyinfo.storage.realm.ConfigurationRealm;
+import com.eyinfo.storage_demo.modules.UserModules;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        ConfigurationRealm.getInstance().init(getApplicationContext(),
+        ConfigurationRealm.getInstance().init(
+                getApplicationContext(),
                 "storage.realm",
-                new UserModules());
+                new UserModules()
+        );
     }
 }
